b71344bf2c7ef756c240b4dab39aacc084861b00
Johan Sogaard <Johansogaardjorgensen@gmail.com>
2023-09-25 16:44:12 +0200

    Working on login JJ

--- app/src/main/java/com/example/stonks/MainActivity.kt
+++ app/src/main/java/com/example/stonks/MainActivity.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -101,21 +104,28 @@ fun frontPageBtns(modifier: Modifier = Modifier)
 
     )
     {
+            Spacer(modifier = Modifier.weight(2F))
 
-            Button(
+                Button(
                 onClick = { /*TODO*/ },
-                modifier = Modifier.weight(2F)
+                modifier = Modifier
+
+                    .weight(1F)
+                    .fillMaxWidth(0.5f)
                 ) {
                 Text("Login")
+                }
+              Spacer(modifier = Modifier.weight(0.5F))
 
 
-
-            }
             Button(onClick = { /*TODO*/ },
-                modifier = Modifier.weight(2F)) {
+                modifier = Modifier
+                    .weight(1F)
+                    .fillMaxWidth(0.5f)) {
                 Text("Sign up")
 
             }
+             Spacer(modifier = Modifier.weight(2F))
         }
 
 }
